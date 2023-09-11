c0f16601fc9e29fa3d0686d64df65b14cce6ed81
solomon975135 <saarah.solomon@gmail.com>
2023-09-11 22:40:29 +0200

    planning mine generation

--- src/main/java/io/github/solomon135975/MineGenerator.java
+++ src/main/java/io/github/solomon135975/MineGenerator.java
@@ -1,5 +1,18 @@
 package io.github.solomon135975;
 
+import java.util.Random; 
+
+/* idea: 
+ * randomised depth first search:
+ * 1. starting point: player chooses first cell
+ * 2. ensure this cell is not a mine (it isn't because we haven't set any yet), mark as revealed
+ * 3. start dfs from this first cell, mark immediate surrounding cells as revealed as well (they will not be bombs)
+ * 4. from the neighbours, start the dfs and place mines in them or from them etc
+ * 5. continue placing bombs until max num bombs reached
+ * 
+ * when a mine is placed, increment value of non-mine cells around it
+ */
+
 public class MineGenerator {
-    //generate mines - RDFS 
+    
 }
---
+++ src/main/java/io/github/solomon135975/PlayMinesweeps.java
@@ -0,0 +1,14 @@
+package io.github.solomon135975;
+
+/**
+ * play game
+ *
+ */
+public class PlayMinesweeps 
+{
+    public static void main( String[] args )
+    {
+        Grid grid = new Grid(5,5,1);
+        grid.printGrid();
+    }
+}
